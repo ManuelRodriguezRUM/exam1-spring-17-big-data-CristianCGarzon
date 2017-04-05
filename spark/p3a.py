2d1ac7dcf554c936fd9071a2283a0581c88b44a5
Cristian Camilo Garzon Alfonso <garzoncristianc@gmail.com>
2017-04-04 20:00:59 -0700

    Spark part

--- spark/p3a.py
+++ spark/p3a.py
@@ -1,1 +1,5 @@
 #Code here
+studentsOriginal = sc.textFile('/cristian/examBigData/hive/studentsPR.csv')
+studCsv = studentsOriginal.map(lambda line : line.split(','))
+result = studCsv.filter(lambda sex : sex[5] == 'F').filter(lambda code : code[2] == '71381')
+result.collect()
--- spark/p3b.py
+++ spark/p3b.py
@@ -1,1 +1,5 @@
 #Code here
+schoolsOriginal = sc.textFile('/cristian/examBigData/hive/escuelasPR.csv')
+schoolsCsv = schoolsOriginal.map(lambda line : line.split(','))
+result = schoolsCsv.filter( lambda city: city[2] == 'Ponce' or city[2] == 'Cabo Rojo' or city[2] == 'Dorado')
+result.collect()
